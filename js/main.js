ad947423f903dcfc864549c0768731be0ed6b7cd
MNxMunno <bilolhuzayfa@gmmail.com>
2024-03-26 01:00:41 +0500

    change

--- js/main.js
+++ js/main.js
@@ -40,10 +40,34 @@ function createCard(data) {
         </div>
         <h3 title="${product.title}">${product.title}</h3>
         
+        </div>
+        <div class="price-rate">
         <p class="price">${product.price} $</p>
+        <p class="rating">⭐ ${product.rating.rate}</p>
+        </div>
+        <p class="description" title="${product.description}">${product.description} </p>
+        <p class="category"><b>Category:</b>  ${product.category} </p>
+        <div class="card-btns">
+        <button class="sale">Купить сейчас</button>
+        <button class="btn-cart">
+        <svg class="cart" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" transform: ;msFilter:;"><path d="M21.822 7.431A1 1 0 0 0 21 7H7.333L6.179 4.23A1.994 1.994 0 0 0 4.333 3H2v2h2.333l4.744 11.385A1 1 0 0 0 10 17h8c.417 0 .79-.259.937-.648l3-8a1 1 0 0 0-.115-.921z"></path><circle cx="10.5" cy="19.5" r="1.5"></circle><circle cx="17.5" cy="19.5" r="1.5"></circle></svg>
+        </button>
+        </div>
         `;
 
     fragment.appendChild(card);
   });
   cards.appendChild(fragment);
 }
+
+/////////////// F O R M /////////////
+
+const form = document.querySelector(".form");
+const search = document.querySelector("#search");
+const btnSubmit = document.querySelector(".btn-submit");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  btnSubmit.addEventListener("click", () => {
+    search.value = "";
+  });
+});
